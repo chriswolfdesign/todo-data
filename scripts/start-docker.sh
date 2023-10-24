e6ded7209a3d072b9c032567e16250071a04e3ca
Chris Wolf <chriswolfdesign@gmail.com>
2023-10-24 19:16:51 -0400

    Adds network creation to start docker script

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 source vars/env_vars.env
+docker network create todo
 docker run --network todo --name todo-data -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -e POSTGRES_USER=${POSTGRES_USER} -v pgdata:/var/lib/postgresql/data -d postgres
