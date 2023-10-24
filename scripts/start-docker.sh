e7d0cc383c2636709e7653753614f69e167eb8a2
Chris Wolf <chriswolfdesign@gmail.com>
2023-10-24 19:02:18 -0400

    Update to expect backend to be on docker network "todo"

--- scripts/start-docker.sh
+++ scripts/start-docker.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 
 source vars/env_vars.env
-docker run --name todo-data -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -e POSTGRES_USER=${POSTGRES_USER} -p 5432:5432 -v pgdata:/var/lib/postgresql/data -d postgres
+docker run --network todo --name todo-data -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} -e POSTGRES_USER=${POSTGRES_USER} -v pgdata:/var/lib/postgresql/data -d postgres
